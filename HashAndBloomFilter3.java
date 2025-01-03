799b134ba65e9891caf9ae59e7e3137a4034b725
Goodrat-a <sms298121@gmail.com>
2025-01-03 20:30:45 +0330

    Bloom search implementation

--- HashAndBloomFilter3.java
+++ HashAndBloomFilter3.java
@@ -23,7 +23,9 @@ public static void containsInBloomFilter(ArrayList<String> phrases,String entry,
         HashMap<Integer, String> hashMap = new HashMap<>();
     
             BitSet bloomFilter = new BitSet(SIZE);
-
+             int inputHash1 = hash(entry, SEED1);
+             int inputHash2 = hash(entry, SEED2);
+            if (choice==3) {
             for (String phrase : phrases) {
                 int hash1 = hash(phrase, SEED1);
                 int hash2 = hash(phrase, SEED2);
@@ -34,7 +36,26 @@ public static void containsInBloomFilter(ArrayList<String> phrases,String entry,
                 bloomFilter.set(hash2);
             }
 
-            else {
+            long start = System.nanoTime();
+            if (bloomFilter.get(inputHash1) && bloomFilter.get(inputHash2)) {
+                long end = System.nanoTime();
+                System.out.println("Possibly in the list (Bloom Filter).");
+                System.out.println("test time is :"+(end-start)/pow(10,4));
+
+                
+                if (hashMap.containsKey(inputHash1) && hashMap.get(inputHash1).equals(entry)) {
+                    System.out.println("Definitely found in the list (HashMap).");
+                } else {
+                    System.out.println("False positive detected.");
+                }
+            } else {
+                long end2 =System.nanoTime();
+                System.out.println("Definitely not in the list.");
+                System.out.println("test time is :"+(end2-start)/pow(10,4));
+            }
+        }
+
+        else{
             for (String phrase : phrases) {
                 int hash1 = hash(phrase, SEED1);
 
@@ -49,6 +70,6 @@ public static void containsInBloomFilter(ArrayList<String> phrases,String entry,
             long end1 = System.nanoTime();
             System.out.println("test time is :"+(end1-start1)/pow(10,4));
         }
-            
+}        
         
 }
