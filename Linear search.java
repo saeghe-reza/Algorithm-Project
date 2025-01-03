22a0cdbfa2f3de70d2fdeda444e0facf93c3d631
saeghe-reza <1380rezasaeghe@gmail.com>
2025-01-03 17:34:12 +0330

    Add comments

--- Linear search.java
+++ Linear search.java
@@ -1,27 +1,27 @@
 public class LinearAndBinarySearch{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-       
+        //مسیر فایل رو انتخاب میکنیم
         String filePath = "C:\\Users\\hp\\Downloads\\usernames.txt";
         ArrayList<String> database = new ArrayList<>();
-        
+        //تعریف فایل ریدر برای خواندن فایل
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
                 database.add(line);
             }
-            
+            //اظافه کردن هر خط به لیست ارایه ی database
         } catch (IOException e) {
             e.printStackTrace();
         }
-       
+        //مرتب سازی
         Collections.sort(database);
-        
+        //گرفتن ورودی
         System.out.print("plz enter your desired name:");
         String target = in.nextLine();
         System.out.print("what are you want to use\n1.linear search:\n2.binary search\n3.hashAndBloomFilter:");
-        
-               
+            //جستجوی خطی
+                //پیمایش تمام اعضا از ابتدا
                 long start = System.nanoTime();
                 for (int i = 0; i < database.size(); i++) {
                     if (database.get(i).equals(target)) {
