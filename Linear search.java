19b19644a03f88ea863c2203fe560a72636f16a4
saeghe-reza <1380rezasaeghe@gmail.com>
2025-01-03 17:42:53 +0330

    Implementation of binary search function

--- Linear search.java
+++ Linear search.java
@@ -37,3 +37,30 @@ public class LinearAndBinarySearch{
                     }
                 
             }
+
+
+ public static int binarySearch(ArrayList<String> database, String target) {
+       
+        int left = 0;
+        
+        int right = database.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int comparison = target.compareTo(database.get(mid));
+
+            if (comparison == 0) {
+                
+                return mid;
+            }
+            
+            if (comparison > 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
